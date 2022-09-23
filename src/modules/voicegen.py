b381f931ddde079781f5fd01133996e73ed253c0
Miguel Estrada <miguelestradam36@estudiantec.cr>
2022-09-23 02:18:12 -0600

    Updating python scripts

--- src/modules/voicegen.py
+++ src/modules/voicegen.py
@@ -1,21 +1,21 @@
 class RobVoiceGen():
+
     pyttsx3 = __import__('pyttsx3')
     os = __import__('os')
+
     output_loc = ''
     current_text = ''
+    
     def __init__(self):
         print("Starting voice conversion...")
-        self.start_engine()
-        
-    def start_engine(self):
-        engine = self.pyttsx3.init()
+        self.engine = self.pyttsx3.init()
 
     @property
     def voice_conf(self):
         return self.upload_file_state_
 
     @voice_conf.setter
-    def voice_conf(self, volume:int):
+    def voice_conf(self, volume:int, gender:int, ):
         voices = self.engine.getProperty('voices')
 
     @property
