6fc721f77ad56f83606da70e9efcdfb8d9993179
Miguel Estrada <miguelestradam36@estudiantec.cr>
2022-12-01 12:31:33 -0600

    Modifying script

--- src/main.py
+++ src/main.py
@@ -5,6 +5,6 @@ if __name__  == "__main__":
     buffer.voice_rate_conf = -7
     buffer.output_conf = r'output/example.mp3'
     buffer.text_script_conf = r'scripts/text.txt'
-    buffer.run_speech()
+    #buffer.run_speech()
     #buffer.voices_try_out()
     buffer.save_to_mp3()
